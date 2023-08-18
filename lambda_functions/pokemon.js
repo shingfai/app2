df6d38e48c5217dbad1c5f0ed34b1e288fd3408f
shingfai <69297550+shingfai@users.noreply.github.com>
2023-08-18 22:10:08 +0800

    1

--- lambda_functions/pokemon.js
+++ lambda_functions/pokemon.js
@@ -59,3 +59,21 @@ module.exports.handler = async (event, context) => {
       return { statusCode: 400 };
   }
 };
+
+
+// test script
+
+const postRequest = await fetch("/.netlify/functions/pokemon", {
+  method: "POST",
+  body: JSON.stringify({
+    name: "Pikachu",
+    number: 25,
+  }),
+});
+
+console.log("POST request status code", postRequest.status);
+
+const newGetRequest = await fetch("/.netlify/functions/pokemon");
+const newListJson = await newGetRequest.json();
+
+console.log("GET request new result", newListJson);
